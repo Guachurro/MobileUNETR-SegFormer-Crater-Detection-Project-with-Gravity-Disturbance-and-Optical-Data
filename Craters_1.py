b8450813b540041d381155376eb6442475e7c668
Guachurro <165832547+Guachurro@users.noreply.github.com>
2024-08-31 16:53:16 -0400

    Update Craters_1.py

--- Craters_1.py
+++ Craters_1.py
@@ -4,7 +4,7 @@ Created on Mon Mar 25 11:41:12 2024
 
 @author: Guach
 """
-#Code to read the crater database and create a georeferenced set of polygons from it. 
+#Necessary modules and file destinations
 import geopandas as gpd
 
 import pandas as pd
@@ -46,55 +46,79 @@ kmdeg=ppd*kmpix
 #Degrees per kilometer
 dkm=kmdeg**(-1)
 
-#Moon2000 Equidistant Cylindrical (Uses meters as the units) 
+#Moon2000 Equidistant Cylindrical (Uses meters as the units)
 crs1="Moon_2000_Equidistant_Cylindrical"
 
-######Defining the ellipsoid used by the Shapefile, which is the dataset we're doing calculations on.(Not going to be used for now) 
-#a=1737.8981*1000 #Equatorial radius
-#b=1737.1513*1000 #Polar Radius
-#f=(a-b)/a
-#e2=(1-b**2)/a**2
-#geod=Geod(ellps="Moon_2000_IAU_IAG")
+###SLDEM2015
+Moon="F:/Academics/OSU/Thesis Documents/Lunar DEMs/SLDEM2015_256_60S_60N_000_360.JP2"
 
-#DEM
+###Gravity Map GGGrx1200a
+MoonGrav="F:/Academics/OSU/Thesis Documents/Lunar Gravity Maps/gggrx_1200a_dist_l1200.tif"
+
+###Output Place for file generation
+#This one stores the labels for Crater images
+LabelOut="F:/Academics/OSU/Thesis Documents/Generated Images/Labels/"
+#This stores the crater image clips as taken from Gravity Map without altering pixel values. 
+CraterOut="F:/Academics/OSU/Thesis Documents/Generated Images/Crater Gravity Images/"
+#This is the label image
+GravClip="F:/Academics/OSU/Thesis Documents/Generated Images/Labels/AoI/0N_13E_5kR_Craters.tif"
+
+
 #Run this to get all the files in place
-#%% 
 print('Reading files')
-#Craters Shapefile (This was created from the Robbins 2018 Database).
-Shape=gpd.read_file("F:\Academics/OSU/Moon Related Projects/Crater Detection Project/Lunar Crater Database/Robbins database USGS 2018/Catalog_Moon_Release_20180815_shapefile180/Catalog_Moon_Release_20180815_1kmPlus_180.shp")
-#Proj=gpd.read_file("F:\Academics\OSU\Moon Related Projects\Crater Detection Project\Lunar Crater Database\Robbins database USGS 2018\Catalog_Moon_Release_20180815_shapefile180\Catalog_Moon_Release_20180815_1kmPlus_180.prj")
-#Elevation Map
-Moon="F:/Academics/OSU/Moon Related Projects/Global/SLDEM2015_256_60S_60N_000_360.JP2"
-#Gravity disturbance product
-MoonGrav="F:/Academics/OSU/Moon Related Projects/gggrx_1200a_dist_l1200.tif"
+####Craters Shapefile (This was created from the Robbins 2018 Database).
+#If k=1 Then the Shapefile has already been edited to the size we want so we read the smaller version
+k=1
+if k==1:
+    Shape=gpd.read_file("F:/Academics/OSU/Thesis Documents/CratersAOI/CratersAOI.shp")
+else: 
+    Shape=gpd.read_file("F:/Academics/OSU/Thesis Documents/Crater Databases/Robbins database USGS 2018/Catalog_Moon_Release_20180815_shapefile180/Catalog_Moon_Release_20180815_1kmPlus_180.shp")
 
 #CRS of the DEM. Geographic 2D CRS
 CRS_DEM=rasterio.open(MoonGrav).crs
 CRS_SH=Shape.crs
 
 #Create a Polygon that will be the Area of Interest to acquire craters
+print('Create AOI Box')
 MinLat=-20
 MaxLat=20
 MinLong=3
 MaxLong=23 
 AoI=Polygon(((MinLong,MinLat),(MaxLong,MinLat),(MaxLong,MaxLat),(MinLong,MaxLat)))
-#%%
-#In this Block we'll be removing all the craters that aren't centered in our desired AoI
-list=[]
-for index, row in Shape.iterrows():
-    if AoI.contains(Shape['geometry'][index]):
-        #If its contained you're good. Added value for debugging
-        print(Shape['geometry'][index])
-    else:#Add the index value to a list so we can drop them all later from the Geodataframe
-        list.append(index)
 
-Shape=Shape.drop(list)
-print('Finished eliminating undesired rows')
-#%% Need to change this block later to use pyproj instead. Must figure out how to designate my own Ellipsoid information, as Pyproj doesn't currently have Moon based ellipsoids.
+
+#Eliminate non AOI Craters and create radius
+#Skip when k=0
+if k==1:
+    list=[]
+    for index, row in Shape.iterrows():
+        if AoI.contains(Shape['geometry'][index]) :
+            #If its contained you're good. Added value for debugging
+            print(Shape['geometry'][index])
+        else:#Add the index value to a list so we can drop them all later from the Geodataframe
+            list.append(index)
+
+    Shape=Shape.drop(list)
+    list=[]
+    print('Eliminated Craters outside of AOI')
+    print('Creating Crater Radius')       
+    #####Crater radius defined as half of the diameter of a circular fit. Original is in kilometers, we need it in meters.  
+    Shape['Radius']=(Shape['DIAM_C_IM']/2)*1000
+    print('Eliminating unneccessary columns')
+    Shape=Shape.drop(columns=Shape.iloc[:,1:17])
+    
+    
+#Need to change this block later to use pyproj instead. Must figure out how to designate my own Ellipsoid information, as Pyproj doesn't currently have Moon based ellipsoids.
+#Likewise, if we have already created the edited database we dont need to repeat these steps. We skip the whole block. 
 #####Prepare a polygon for each Crater that will be used to label and extract images later on
-print('Creating Crater Polygons')       
-#####Crater radius defined as half of the diameter of a circular fit. Original is in kilometers, we need it in meters.  
-Shape['Radius']=(Shape['DIAM_C_IM']/2)*1000
+###Eliminate craters with less than 5k meter radius (10 kilometers)
+list=[]
+print('Preparing list of craters')
+for index, row in Shape.iterrows():
+    if Shape['Radius'][index]<5000:
+        list.append(index)
+Shape=Shape.drop(list)
+print('Eliminated Craters with radius criteria')
 #####Project into cylindrical system because Shapely will perform operatons assuming a flat surface. There will be errors? 
 print('Projecting into Cylindrical system')
 Shape=Shape.to_crs(crs1)
@@ -102,29 +126,45 @@ print('Creating crater polygons')
 Shape['Boundaries']=Shape['geometry'].buffer(Shape['Radius'])
 print('Projecting back into Geographic')
 Shape=Shape.to_crs(CRS_SH)
-print('Created GDF. Dismissing unwanted data. ')
-#####Remove unwanted data from variable
-Shape=Shape.drop(columns=Shape.iloc[:,1:17])
-print('Creating AoI Polygon')
+Shape['Boundaries']=Shape['Boundaries'].to_crs(CRS_SH)
+
+print('Preparing to clip Gravity Map')
+TIF=rasterio.open(MoonGrav)
+####Create the mask using only the chosen craters. 
+out_image, out_transform = rasterio.mask.mask(TIF, Shape['Boundaries'], crop=True)
+meta=TIF.profile
+meta.update({"driver":TIF.driver,
+             "height":out_image.shape[1],
+             "width":out_image.shape[2],
+             "transform":out_transform})
+#%%
+print('Saving Clip')
+#Save as GeoTiff
+output_dir=LabelOut
+filename=os.path.join(output_dir+"/AoI/"+"0N_13E_5kR_Craters.tif")
+with rasterio.open(filename, "w",**meta) as dest:
+    dest.write(out_image)
+
 #%%
 #This block will generat ethe images and labels of craters given a GeoDataFrame
 print('Moving into loop')
 #Obtain all craters in desired area with a radius of 10 kilometers or more
 for index, row in Shape.head(5).iterrows():
     with rasterio.open(MoonGrav) as src:
-        with rasterio.open(Moon) as src1:
+        with rasterio.open(GravClip) as src1:
             ####Boundaries of the polygon containing the crater
             MinLat=row['Boundaries'].bounds[1]
             MaxLat=row['Boundaries'].bounds[3]
             MinLong=row['Boundaries'].bounds[0]
             MaxLong=row['Boundaries'].bounds[2]
+            CraterID=row['CRATER_ID']+'.tif'
             
             ####Lat/Long Offset by calculating their total extent and dividing it by 2. That number is then used to shift the window by applying a random percentage from 1 to 100
             # (Max - Min)/2 gets us half of the total window span in degrees
             # (random.randint(1,100)/100) Generates a random Integer from 1 to 100 that is turned into a percentage. This is multiplies by the window span to know how much we're moving from target center.
             # ((-1)**random.randint(1,100)) Will determine a positive or negative value that will determine whether the shift is positive or negative
             LatOffset=((MaxLat-MinLat)/2)*(random.randint(1,100)/100)*((-1)**random.randint(1,2))
-            LonOffset=((MaxLong-MinLong)/2)*(random.randint(1,100)/100)((-1)**random.randint(1,2))
+            LonOffset=((MaxLong-MinLong)/2)*(random.randint(1,100)/100)*((-1)**random.randint(1,2))
                 
             # Min and Max values are both offset by the same ammount to maintain the same original window size. 
             Minlat=MinLat+LatOffset
@@ -132,37 +172,35 @@ for index, row in Shape.head(5).iterrows():
             MinLong=MinLong+LonOffset
             MaxLong=MaxLong+LonOffset
             
-            ####Create a bounding box that has been offset from the Crater in some direction 
+            ####Create a bounding box that has been offset from the Crater in some direction. 
             BoundingBox=Polygon(((MinLong,MinLat),(MaxLong,MinLat),(MaxLong,MaxLat),(MinLong,MaxLat)))
             
-            ####Making sure this is empty just in case
-            im=[]
-            ####This output is the cropping of te Gravity Data
+            ####This output is the cropping of te Gravity Data.
             out_image, out_transform = rasterio.mask.mask(src,[BoundingBox], crop=True)
-            ####This output is the cropping of the Lunar DEM (SLDEM2015)
+            meta=src.profile
+            meta.update({"driver":src.driver,
+                         "height":out_image.shape[1],
+                         "width":out_image.shape[2],
+                         "transform":out_transform})
+            print(meta)
+            filename=os.path.join(CraterOut+CraterID)
+            with rasterio.open(filename, "w",**meta) as dest:
+                dest.write(out_image)
+            
+            
+            ####This output is the cropping of the Label. 
             out_image1, out_transform1 = rasterio.mask.mask(src1,[BoundingBox], crop=True)
-            ####Remove first dimension and make Nodata values equal to 0
-            im=numpy.squeeze(out_image)
-            im[im==-32767]=0
-            im1=numpy.squeeze(out_image1)
-            im1[im1==-32767]=0
-            
-            #Create plot GRAVITY
-            plt.imshow(im, cmap='viridis')
-            plt.title(row['CRATER_ID'])
-            #Show plot
-            plt.show()
-
-            #Create plot Crater DEM (Meant to serve as a debugging feature)
-            plt.imshow(im1, cmap='gist_gray')
-            plt.title(row['CRATER_ID'])
-            #Show plot
-            plt.show()                    
-            
-            #The next step is to recreate each Gravity Map cropping and properly label it. 
-                    #Save as GeoTiff
-                    #output_dir="F:/Academics/OSU/Moon Related Projects/CraterImages"
-                    #filename=os.path.join(output_dir,f"{row['CRATER_ID']}.tif")
-                    #with rasterio.open(filename, "w",**src.profile) as dest:
-                     #   dest.write(out_image)
+            out_image1[out_image1!=-32767]=1
+            meta1=src1.profile
+            meta1.update({"driver":src1.driver,
+                        "height":out_image1.shape[1],
+                        "width":out_image1.shape[2],
+                        "transform":out_transform1})
+            print(meta1)            
+            filename=os.path.join(LabelOut+"/Individual Labels/"+CraterID)
+            with rasterio.open(filename, "w",**meta1) as dest:
+                dest.write(out_image1)
+                
+            print('Crater done')
 print('Exit loop')
+
